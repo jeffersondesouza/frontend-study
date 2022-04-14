b00967388089ba0e9e93a7dcf0a06a0f3a9bc173
Jefferson <jeffersondesouzacordel@gmail.com>
2022-04-14 15:48:24 -0300

    update project

---
+++ next-js-1/pages/faq.js
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1>FAQ!</h1>
+      <a href="/">Home</a>
+      <a href="/faq">faq page</a>
+    </div>
+  );
+}
---
+++ next-js-1/pages/index.js
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Alura case!</h1>
+      <a href="/">Home</a>
+      <a href="/faq">faq page</a>
+    </div>
+  );
+}
