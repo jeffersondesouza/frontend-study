d43e448a37cc355a0a4890687850f24b11e1635e
Jefferson <jeffersondesouzacordel@gmail.com>
2022-04-16 16:42:58 -0300

    update project

--- next-js-1/pages/index.js
+++ next-js-1/pages/index.js
@@ -1,13 +1,3 @@
-import Link from "../src/components/Link";
+import HomeScreen from "../src/screens/HomeScreen";
 
-export default function HomePage() {
-  return (
-    <div>
-      <h1>Alura case!</h1>
-      <nav>
-        <Link href="/">Home</Link>
-        <Link href="/faq">faq page</Link>
-      </nav>
-    </div>
-  );
-}
+export default HomeScreen;
