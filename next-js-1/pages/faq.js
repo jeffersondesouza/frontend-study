3304eafcc6a7b9be42aef22632dc144fa1f7791f
Jefferson <jeffersondesouzacordel@gmail.com>
2022-04-19 17:11:43 -0300

    update project: load props 1

--- next-js-1/pages/faq.js
+++ next-js-1/pages/faq.js
@@ -1,9 +1,42 @@
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
-export default function HomePage() {
+export async function getStaticProps() {
+  return {
+    props: {
+      hello: "Hello world",
+    },
+  };
+}
+
+export default function HomePage(props) {
+  console.log(props)
+  const [faq, setFaq] = useState([]);
+  useEffect(() => {
+    const FAQ_API_URL =
+      "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json";
+
+    fetch(FAQ_API_URL)
+      .then((res) => res.json())
+      .then((res) => setFaq(res));
+  }, []);
   return (
     <div>
       <h1>FAQ!</h1>
+      <div>
+        <ul>
+          {faq.map((item) => (
+            <li key={item.question}>
+              <article>
+                <div>
+                  <strong>{item.question}</strong>
+                </div>
+                <div>{item.answer}</div>
+              </article>
+            </li>
+          ))}
+        </ul>
+      </div>
       <nav>
         <Link href="/">Home</Link>
         <Link href="/faq">faq page</Link>
